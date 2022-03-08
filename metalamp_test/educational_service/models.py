a8a55b9ff7d6f199984851bf08e27e6b0a34aab2
InfSuperStar <infiwow777@yandex.ru>
2022-03-08 18:06:12 +0700

    check for theme.is_publish added

--- metalamp_test/educational_service/models.py
+++ metalamp_test/educational_service/models.py
@@ -64,6 +64,7 @@ class Theme(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     slug = models.SlugField(max_length=100, unique=True, db_index=True)
+    is_published = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title.__str__()
